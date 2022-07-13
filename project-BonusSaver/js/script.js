3f87e7388e95f2424dc746c7bea803a1d71531b8
CarlosJunioor <carlosjunior866@gmail.com>
2022-07-13 20:01:32 +0100

    Update script.js

--- project-BonusSaver/js/script.js
+++ project-BonusSaver/js/script.js
@@ -1,20 +1,19 @@
 const tempString = `
-</div>
-<ul class="task-box" id="taskBox">
-    <li>Test 1</li>
-    <li>Test 2</li>
-    <li>Test 3</li>
-    <li>Test 4</li>
-</ul>
-</div>`
+            </div>
+            <ul class="task-box" id="taskBox">
+                <li>Test 1</li>
+                <li>Test 2</li>
+                <li>Test 3</li>
+                <li>Test 4</li>
+            </ul>
+            </div>
+`
 
 const addString = `<br>add this string `
 
 function render(){
-    
     console.log('[render]')
     document.querySelector(".screen").innerHTML = tempString
-
 }
 
 function deleteTask (){
@@ -22,8 +21,6 @@ function deleteTask (){
 }
 
 function writeTask (){
-
-
     document.querySelector(".screen").innerHTML = addString
 }
 render();
